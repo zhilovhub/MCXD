116ddb65e54e66471d50962ab4e8316d76571b6d
Ilya Zhilov <zhilovgithub@gmail.com>
2022-10-11 17:35:30 +0300

    finish

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import string
-import requests
-from time import sleep
-from json import loads, dump
+import aiohttp
+from json import loads
+
+from aiogram import Dispatcher, Bot, executor, types
+import config
+import aioschedule
+import asyncio
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
 }
 
 
-def get_currency_koronapay() -> list:
+async def get_currency_koronapay() -> list:
     url = "https://koronapay.com/transfers/online/api/transfers/tariffs"
 
     params = {
@@ -19,7 +23,7 @@ def get_currency_koronapay() -> list:
         "paymentMethod": "debitCard",
         "receivingAmount": 500,
         "receivingMethod": "cash",
-        "paidNotificationEnabled": True
+        "paidNotificationEnabled": "true"
     }
 
     currency_dict = {
@@ -31,39 +35,65 @@ def get_currency_koronapay() -> list:
     currencies_values = []
     for currency_name in currency_dict.keys():
         params["receivingCurrencyId"] = currency_dict[currency_name]
-        response = requests.get(url, headers=headers, params=params).json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers, params=params) as response:
+                response = await response.json()
 
-        currencies_values.append(response[0]["exchangeRate"])
-        sleep(1)
+            currencies_values.append(response[0]["exchangeRate"])
+            await asyncio.sleep(1)
 
     return currencies_values
 
 
-def get_currency_garantex() -> float:
-    response = requests.get("https://garantex.io/trading/usdtrub", headers=headers).text
-    data = loads(response[response.find("//<![CDATA[") + 11:response.find("//]]>")]
-                 .strip().strip("window.gon = ").strip(";"))
+async def get_currency_garantex() -> float:
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://garantex.io/trading/usdtrub", headers=headers) as response:
+            response = await response.text()
+            data = loads(response[response.find("//<![CDATA[") + 11:response.find("//]]>")]
+                         .strip().strip("window.gon = ").strip(";"))
 
-    with open("data.json", "w", encoding="utf-8") as f:
-        dump(data, f, indent=4, ensure_ascii=False)
+    # with open("data.json", "w", encoding="utf-8") as f:
+    #     dump(data, f, indent=4, ensure_ascii=False)
 
     garantex_usd = float(data["tickers"]["usdtrub"]["sell"])
     return garantex_usd
 
 
-def main() -> string:
-    koronapay_usd, koronapay_eur, koronapay_try = get_currency_koronapay()
-    garantex = get_currency_garantex()
+async def main() -> string:
+    koronapay_usd, koronapay_eur, koronapay_try = await get_currency_koronapay()
+    garantex = await get_currency_garantex()
     # print(koronapay_usd)
     # print(koronapay_eur)
     # print(koronapay_try)
     # print(get_currency_garantex())
     spread_usd = (max(garantex, koronapay_usd) - min(garantex, koronapay_usd)) / (max(garantex, koronapay_usd)) * 100
     spread_eur = (max(garantex, koronapay_eur) - min(garantex, koronapay_eur)) / (max(garantex, koronapay_eur)) * 100
-    
-    
+
     mess = f'Курс короны в турцию 🇹🇷 \n Доллар 💵 = {"{0:.2f}".format(koronapay_usd)} \n Евро 💶 = {"{0:.2f}".format(koronapay_eur)} \n Лира 💷 = {"{0:.2f}".format(koronapay_try)} \n Garantex USDT 💰 = {garantex} \n Грязный спред по $ = {"{0:.2f}".format(spread_usd)}% \n Грязный спред по € = {"{0:.2f}".format(spread_eur)}%'
     return mess
 
+
+async def send_messages() -> None:
+    await bot.send_message(config.id, await main())
+
+
+async def create_aioschedule() -> None:
+    aioschedule.every(180).seconds.do(send_messages)
+
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(0.5)
+
+
+async def create_tasks(message: types.Message):
+    tasks = [
+        create_aioschedule()
+    ]
+    await asyncio.gather(*tasks)
+
+
 if __name__ == '__main__':
-    main()
+    bot = Bot(token=config.token)
+    dp = Dispatcher(bot)
+
+    executor.start_polling(dp, on_startup=create_tasks)
