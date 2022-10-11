f7db50bcdf9f1c18f6c7943b02d68243666a1d96
Dragzet <arharit27@gmail.com>
2022-10-11 16:55:06 +0300

    done

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def send_messages():
     await bot.send_message(config.id, main())
 
 async def create_aioschedule() -> None:
-    aioschedule.every(2).seconds.do(send_messages)
+    aioschedule.every(180).seconds.do(send_messages)
     
     while True:
         await aioschedule.run_pending()
@@ -24,7 +24,6 @@ async def create_tasks(message: types.Message):
     tasks = [
         create_aioschedule()
     ]
-    
     await asyncio.gather(*tasks)
 
 executor.start_polling(dp, on_startup=create_tasks)
