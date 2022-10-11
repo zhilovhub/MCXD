567c20661ebcdcf97d1ba9df0e883565badaa898
Ilya <zhilovgithub@gmail.com>
2022-10-11 14:13:04 +0300

    get_currency_koronapay

--- main.py
+++ main.py
@@ -1,4 +1,49 @@
 import requests
+from time import sleep
 
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(requests.get("https://garantex.io/trading/usdtrub").text)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+}
+
+
+url = "https://koronapay.com/transfers/online/api/transfers/tariffs"
+
+
+def get_currency_koronapay() -> list:
+    currency_dict = {
+        "USD": 840,
+        "EUR": 978,
+        "TRY": 949
+    }
+
+    params = {
+        "sendingCountryId": "RUS",
+        "sendingCurrencyId": 810,
+        "receivingCountryId": "TUR",
+        "receivingCurrencyId": None,
+        "paymentMethod": "debitCard",
+        "receivingAmount": 500,
+        "receivingMethod": "cash",
+        "paidNotificationEnabled": True
+    }
+
+    currencies_values = []
+    for currency_name in currency_dict.keys():
+        params["receivingCurrencyId"] = currency_dict[currency_name]
+        response = requests.get(url, headers=headers, params=params).json()
+
+        currencies_values.append(response[0]["exchangeRate"])
+        sleep(1)
+
+    return currencies_values
+
+
+def main() -> None:
+    koronapay_usd, koronapay_eur, koronapay_try = get_currency_koronapay()
+    print(koronapay_usd)
+    print(koronapay_eur)
+    print(koronapay_try)
+
+
+if __name__ == '__main__':
+    main()
