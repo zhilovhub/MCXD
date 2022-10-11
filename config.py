a2de7e36377a3e2313d77b07aaef38ac02aaae11
Dragzet <arharit27@gmail.com>
2022-10-11 16:31:52 +0300

    бот есть

---
+++ config.py
@@ -0,0 +1,2 @@
+token = '5536614783:AAEgkOkiE0zHprjx7aZHb99dyLF_EsHGSD4'
+id = '811794316'
--- main.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(requests.get("https://garantex.io/trading/usdtrub").text)
